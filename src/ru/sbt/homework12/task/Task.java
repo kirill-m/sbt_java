de758c739930a00daf5187d386472087a9820197
kirill-m <kmatveev15@gmail.com>
2016-08-29 14:13:05 +0300

    hw12 task done

--- src/ru/sbt/homework12/task/Task.java
+++ src/ru/sbt/homework12/task/Task.java
@@ -8,28 +8,30 @@ import java.util.concurrent.Callable;
 public class Task<T> {
     private final Object lock = new Object();
     private final Callable<? extends T> callable;
-    private T cache = null;
+    private volatile T cache = null;
     private volatile Exception exception = null;
 
     public Task(Callable<? extends T> callable) {
         this.callable = callable;
     }
 
-    public synchronized T get() {
-        if (exception != null)
-            throw new SuddenException("An exception has been thrown", exception);
-
+    public T get() {
         if (cache != null) {
             return cache;
         }
-        T result = null;
-        try {
-            result = callable.call();
-            cache = result;
-            return result;
-        } catch (Exception e) {
-            exception = e;
-            throw new RuntimeException("Unable to compute a result", e);
+        synchronized (lock) {
+            if (exception != null)
+                throw new SuddenException("An exception has been thrown", exception);
+
+            T result = null;
+            try {
+                result = callable.call();
+                cache = result;
+                return result;
+            } catch (Exception e) {
+                exception = e;
+                throw new RuntimeException("Unable to compute a result", e);
+            }
         }
     }
 }
--- src/ru/sbt/homework12/task/TaskExamples.java
+++ src/ru/sbt/homework12/task/TaskExamples.java
@@ -4,7 +4,7 @@ package ru.sbt.homework12.task;
  * Created by kirill on 27.08.16
  */
 public class TaskExamples {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
          Task<Long> task = new Task<>(() -> {
             Long number = 0L;
             for (int i = 0; i < 100_000_000; i++) {
@@ -16,8 +16,10 @@ public class TaskExamples {
          Runnable run = () ->
             System.out.println(task.get() + " " + Thread.currentThread().getName());
 
+
         new Thread(run).start();
         new Thread(run).start();
+        Thread.sleep(2000);
         new Thread(run).start();
         new Thread(run).start();
         new Thread(run).start();
