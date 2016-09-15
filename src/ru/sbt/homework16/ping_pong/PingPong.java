5ad90cf16762cd7a5ef0884c0f70e68a4309837b
kirill-m <kmatveev15@gmail.com>
2016-09-15 12:34:26 +0300

    hw16 ping pong

---
+++ src/ru/sbt/homework16/ping_pong/PingPong.java
@@ -0,0 +1,47 @@
+package ru.sbt.homework16.ping_pong;
+
+/**
+ * Created by kirill on 15.09.16
+ */
+public class PingPong {
+    public static volatile Boolean state = false;
+
+    public static void main(String[] args) {
+        new Thread(new Player(true)).start();
+        new Thread(new Player(false)).start();
+    }
+
+    private static class Player implements Runnable {
+        private final boolean condition;
+        private final String word;
+
+        public Player(boolean condition) {
+            this.condition = condition;
+            word = condition ? "PING" : "PONG";
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (PingPong.class) {
+                    while (state != condition) {
+                        try {
+                            PingPong.class.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(word);
+                    state = !state;
+                    notifyPlayer();
+                }
+            }
+        }
+
+        private void notifyPlayer() {
+            synchronized (PingPong.class) {
+                PingPong.class.notify();
+            }
+        }
+    }
+}
